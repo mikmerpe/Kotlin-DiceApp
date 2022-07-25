5021371e1e47d479ce28971f1a989d8d32fc7dce
Miguel Merenciano <miguelmerenciano@gmail.com>
2022-07-26 01:16:48 +0200

    Lanzamiento de 2 dados

---
+++ app/src/main/java/com/example/diceapp/dice.kt
@@ -0,0 +1,21 @@
+package com.example.diceapp
+
+fun main() {
+    // Primer dado
+    val myFirstDice = Dice(8) // Incializo la instancia de la clase Dice.
+    println("El dado tiene ${myFirstDice.numSides} lados y al lanzarlo ha salido el número ${myFirstDice.roll()}") // Imprimo el numero de lados del dado.
+
+    // Segundo dado
+    val mySecondDice = Dice(20)
+    println("El dado tiene ${mySecondDice.numSides} lados y al lanzarlo ha salido el número ${mySecondDice.roll()}")
+
+
+}
+
+// Creo la clase Dice que representa el dado físico
+class Dice (val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random() // Aqui utilizamos la funcion incluida random() para generar el numero aleatorio a partir del numero de lados del dado.
+    }
+}
